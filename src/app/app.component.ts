1a4d91d257c0a782c33e6b23a264dff24e6c6a79
Can Dincer <dincercan@gmail.com>
2017-10-29 17:23:04 +0300

    Jquery added to project and works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit()
+  {
+    $('button').click(function() { alert('Hi')})
+
+  }
 }
